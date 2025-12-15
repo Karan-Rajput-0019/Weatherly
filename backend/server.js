78a25be58a6dbe21a1972f48b117b067a88bf391
Karan Rajput <3636karan@gmail.com>
2025-12-15 14:58:51 +0530

    karan

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const errorHandler = require('./middleware/errorHandler');
+const errorhandler = require('./middleware/errorhandler');
 
 dotenv.config();
 connectDB();
